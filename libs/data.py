a9eb1aa7d4c4caed3617c26e9fc4cd6723ff40aa
serg <zaswed76@gmail.com>
2015-12-22 14:19:50 +0200

    data.py

---
+++ libs/data.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+
+модуль предоставляет абстрактный класс Data
+класс Data предоставляет "интерфейс"  load
+который загружает базу из файла
+
+в классе потомке требуется перегрузк метода get_data
+
+>>> data = JsonData()
+>>> data.load("path")
+
+'''
+
+import os
+import json
+import shelve
+
+class DataError(Exception): pass
+
+_FILE_ASSERT_TEMPLATE = "файл < {} > не существует"
+_METHOD_ERROR_ASSERT_TEMPLATE = "метод < {} > надо переопределить в наследнике"
+
+
+class Data(dict):
+    def __init__(self):
+        super().__init__()
+
+    def load(self, path):
+        if self:
+            self.clear()
+        if not os.path.isfile(path):
+            raise FileNotFoundError(
+                _FILE_ASSERT_TEMPLATE.format(path))
+
+        self.update(self.get_data(path))
+
+    def get_data(self, path):
+        raise TypeError(_METHOD_ERROR_ASSERT_TEMPLATE.format(Data.get_data.__name__))
+
+
+class JsonData(Data):
+    def get_data(self, path):
+        with open(path, "r") as obj:
+            return json.load(obj)
+
+
+class ShelveData(Data):
+    def get_data(self, path):
+        path = os.path.splitext(path)[0]
+        return shelve.open(path)
+
+
+if __name__ == '__main__':
+    # import paths
+    # json_file = os.path.join(paths.get_data_dir(),
+    #                          "base_geometry_dict.json")
+    # shelve_file = os.path.join(paths.get_data_dir(), "dict_shl",
+    #                            "secondary_geometry_shl.dat")
+    #
+    #
+    # def load(path, data_cls):
+    #     data = data_cls()
+    #     data.load(path)
+    #     print(data)
+    #
+    #
+    # load(json_file, JsonData)
+    # load(shelve_file, ShelveData)
+    import doctest
+    doctest.testmod()
--- test/test.py
+++ test/test.py
@@ -2,21 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-# n = int(input(">> "))
-# result = []
-# for i in range(n + 1):
-#     lst = []
-#     lst.extend(i * [i])
-#     result.extend(lst)
-#
-# print(*result, sep=" ")
+a = {1: 1}
 
-a = int(input(">> "))
-result = []
-for n in range(1, a + 1):
-    seq = [n] * n      #  последовательность из n  n раз
-    result.extend(seq) # добавить seq в результирующий список
-
-print(*result, sep=" ")
-
+if a:
+    print("1")
 
