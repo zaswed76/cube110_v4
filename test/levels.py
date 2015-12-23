38e5f84d84e43800a57b0cde64067c7cb25c799c
serg <zaswed76@gmail.com>
2015-12-24 01:09:50 +0200

    levels

---
+++ test/levels.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Levels:
+    def __init__(self, seq_left, seq_right):
+        self.seq_right = seq_right
+        self.seq_left = seq_left
+
+    def game(self):
+        raise Exception("надо переопределить")
+
+class LevelSort(Levels):
+    def __init__(self, seq_left, seq_right):
+        super().__init__(seq_left, seq_right)
+
+    def play(self):
+        self.seq_left.sort()
+        self.seq_right.sort()
+        print(self.seq_left, "111")
+        print(self.seq_right, "111")
+        print("sort")
+
+class LevelRevers(Levels):
+    def __init__(self, seq_left, seq_right):
+        super().__init__(seq_left, list)
+
+    def play(self):
+        print("revers")
--- test/test_strateg.py
+++ test/test_strateg.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import levels as lv
 
 class Game:
-    level = None
+    current_level = None
+    def __init__(self, levels):
+
+        self.levels = levels
+        self.cursor = 0
+
+    def set_level(self, left, right, level):
+        self.current_level = getattr(lv, level)(left, right)
+
+    def play(self):
+        level_data = self.levels[self.cursor]
+        left, right, level = level_data
+        self.set_level(left, right, level)
+        self.current_level.play()
+        self.cursor +=1
 
 
-class Levels:
-    def
+
+
+if __name__ == '__main__':
+
+    levels = (([1, 2, 3], [1, 2, 3], "LevelSort"),
+              ([5, 6, 7], [5, 6, 7]), "LevelRevers")
+
+    game = Game(levels)
+    game.play()
